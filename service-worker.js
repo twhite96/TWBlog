9ea06c0598bb41264dd7a5ff167d7140f24d5c95
Tiffany White <hey@tiffanyrwhite.com>
2018-10-31 22:55:07 -0400

    Update version for service worker cache

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
   prefix: 'twblog',
-  suffix: 'v6',
+  suffix: 'v7',
   precache: 'precache',
   runtime: 'runtime-cache'
 });
