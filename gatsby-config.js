8883eed987ce4ff61083a06f4bafef582ab24a78
Tiffany White <hey@tiffanyrwhite.com>
2018-12-15 19:22:00 -0500

    Unregister old service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,6 +92,7 @@ module.exports = {
       }
     },
     `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`
     `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-typography',
