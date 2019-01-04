3b662c9b9f8e511579041e5401ec848d91e70864
Tiffany White <hey@tiffanyrwhite.com>
2019-01-04 17:37:23 -0500

    Remove emoji url. Caused build errors

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,10 +3,10 @@ export function formatReadingTime(minutes) {
   return `${ new Array(cups || 1).fill('☕️').join('') } ${ minutes } min read`;
 }
 
-export function emoji() {
-  if (navigator.userAgent.indexOf('Mac OS X') != -1) {
-    window.location.hash = '👩🏾‍💻';
-  }
-}
-emoji();
-console.log("You found my landing page! You're awesome.");
+// export function emoji() {
+//   if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+//     window.location.hash = '👩🏾‍💻';
+//   }
+// }
+// emoji();
+// console.log("You found my landing page! You're awesome.");
