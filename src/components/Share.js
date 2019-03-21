782a55f2350da8120364c5d1897c5b38e30cdcd5
Tiffany White <hey@tiffanyrwhite.com>
2019-03-20 22:59:59 -0400

    Add KCD share buttons with react-share

---
+++ src/components/Share.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import {css} from '@emotion/core'
+import themes from './themes'
+
+import {TwitterShareButton, FacebookShareButton, LinkedinShareButton} from 'react-share'
+
+const Share = ({url, title, twitterHandle}) => (
+  <div
+    css={css`
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      div {
+        margin-right: 20px;
+        cursor: pointer;
+        :hover {
+          color: ${themes.light.header};
+        }
+      }
+      span {
+        margin-right: 20px;
+        font-size: 70%;
+        text-transform: uppercase;
+        line-height: 2.5;
+        opacity: 0.7;
+      }
+    `}
+  >
+    <div
+      css={css`
+        flex-grow: 1;
+        border-top: 1px solid ${themes.dark.background};
+      `}
+    />
+    <span>Share article</span>
+    <TwitterShareButton
+      url={url}
+      title={title}
+      via={twitterHandle.split('@').join('')}
+    >
+      Twitter
+    </TwitterShareButton>
+    <FacebookShareButton
+      url={url}
+      quote={title}
+      via={twitterHandle.split('@').join('')}
+      css={css`
+        cursor: pointer;
+      `}
+    >
+      Facebook
+    </FacebookShareButton>
+    <LinkedinShareButton
+      url={url}
+      quote={title}
+      via={twitterHandle.split('@').join('')}
+      css={css`
+        cursor: pointer;
+      `}
+    >
+      LinkedIn
+    </LinkedinShareButton>
+  </div>
+)
+
+export default Share
