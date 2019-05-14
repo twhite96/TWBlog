89718431c0d23599d5d29cabac752de4ca804de8
Tiffany White <hey@tiffanyrwhite.com>
2019-05-14 11:53:18 -0400

    Update something

--- scripts/newPost.js
+++ scripts/newPost.js
@@ -1,34 +1,31 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const slugify = require('slug');
-const dateFns = require('date-fns');
-
+const fs = require("fs");
+const slugify = require("slug");
+const dateFns = require("date-fns");
 const title = process.argv[2];
 
 if (!title) {
- throw 'a title is required!';
+  throw "a title is required!";
 }
+
 const slug = slugify(title.toLowerCase());
 const date = dateFns.format(new Date(), "YYYY-MM-DD");
-const dir = `./content/posts/${date}---${slug}`;
-
-if (!dir) {
-  throw 'this directory does not exist!';
-}
+const dir = `${__dirname}/content/posts/${date}--${slug}`;
 
 fs.writeFileSync(
   `${dir}/index.md`,
- `---
+  `---
+date: "${date}"
 title: "${title}"
-date: ${date}
-author: "Tiffany White"
-spoiler:
+cover:
+category:
 ---`,
- function(err) {
- if (err) {
- return console.log(err)
- }
- console.log(`${title} was created!`)
- },
+
+  function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`${title} was created!`);
+  }
 );
