73e18e06a43bf0da2fc84088169ebf473782c07e
Tiffany White <hey@tiffanyrwhite.com>
2019-06-10 00:41:39 -0400

    fix syntax highlighting

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "prismjs/themes/prism-gloom.css";
+import '../../utils/global.css'";
+import '../../utils/fonts.css'
 
 import asyncComponent from "../AsyncComponent";
 import Headline from "../Article/Headline";
