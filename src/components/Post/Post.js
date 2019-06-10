878d14aa39789bef6522984f18c6acf2f6481e49
Tiffany White <hey@tiffanyrwhite.com>
2019-06-10 01:20:36 -0400

    fix bug

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import '../../utils/global.css'";
-import '../../utils/fonts.css'
+import "../../utils/global.css";
+import "../../utils/fonts.css";
 
 import asyncComponent from "../AsyncComponent";
 import Headline from "../Article/Headline";
