612c35cbb15e467bc244e5f89de2a77c9b43eac4
Tiffany White <hey@tiffanyrwhite.com>
2019-06-18 23:50:31 -0400

    Update something

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -4,19 +4,25 @@ import { ThemeContext } from "../layouts";
 import Headline from "../components/Article/Headline";
 import Seo from "../components/Seo";
 import Article from "../components/Article";
-import "./style.scss";
-import { FaFlag, FaPlus } from "react-icons/fa";
-import coding from '../images/timeline-images/coding.png'
-import pitt from '../images/timeline-images/pitt.png';
-import dev from '../images/timeline-images/dev.jpg';
-import compass from '../images/timeline-images/compass.png';
+import { FaDev, FaGithub, FaCompass } from "react-icons/fa";
+// import coding from '../images/timeline-images/coding.png'
+// import pitt from '../images/timeline-images/pitt.png';
+// import dev from '../images/timeline-images/dev.jpg';
+// import compass from '../images/timeline-images/compass.png';
 import fire from '../images/timeline-images/fire.png';
 import gatsby from '../images/timeline-images/gatsby.png';
 import egghead from '../images/timeline-images/egghead.jpg';
-import github from '../images/timeline-images/github.png';
-import codesandbox from '../images/timeline-images/codesandbox.jpg';
+// import github from '../images/timeline-images/github.png';
+// import codesandbox from '../images/timeline-images/codesandbox.jpg';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
-
+// const handleClick = (event) => {
+//   event.preventDefault();
+// }
 const TimelinePage = props => {
   const {
     data: {
@@ -27,139 +33,110 @@ const TimelinePage = props => {
   } = props;
 
   return (
-    <React.Fragment>
-      <ThemeContext.Consumer>
-        {theme => (
-          <Article theme={theme}>
-            <header>
-              <Headline title="Timeline of Professional Accomplishments" theme={theme} />
-            </header>
+      <React.Fragment>
+        <ThemeContext.Consumer>
+          {theme => (
+            <Article theme={theme}>
+              <header>
+                <Headline title="Timeline of Professional Accomplishments" theme={theme} />
+              </header>
+              <p>
+                Florin Pop has a timeline of his professional accomplishments on his blog. I decided
+                I wanted to do the same. It's a way for me to see my progress and a way to keep my
+                accountable and chugging along.
+
+               (Switching up timelines so this one is under construction)
+              </p>
+            </Article>
+          )}
+        </ThemeContext.Consumer>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2011 - present"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            icon={<FaDev />}
+          >
+            <h3 className="vertical-timeline-element-title">Creative Director</h3>
+            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
             <p>
-              Florin Pop has a timeline of his professional accomplishments on his blog. I decided I
-              wanted to do the same. It's a way for me to see my progress and a way to keep my
-              accountable and chugging along.
+              Creative Direction, User Experience, Visual Design, Project Management, Team Leading
             </p>
-          </Article>
-        )}
-      </ThemeContext.Consumer>
-      <div class="timeline is-centered">
-        <header class="timeline-header">
-          <span class="tag is-medium is-primary">Start</span>
-        </header>
-        <div class="timeline-item is-warning">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={coding} alt="Code Newbie in Pittsburgh" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">January 2015</p>
-            <p>Started my first ever developer blog, Code Newbie in Pittsburgh.</p>
-          </div>
-        </div>
-        <div class="timeline-item is-warning">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={pitt} alt="Pitt" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">August 2015</p>
-            <p>Started the University of Pittsburgh</p>
-          </div>
-        </div>
-        <header class="timeline-header">
-          <span class="tag is-primary">2016</span>
-        </header>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={dev} alt="DEV" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">October 2016</p>
-            <p>Wrote first post at DEV</p>
-          </div>
-        </div>
-        <header class="timeline-header">
-          <span class="tag is-primary">2017</span>
-        </header>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={compass} alt="YelpCamp" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">October 2017</p>
-            <p>Completed The Web Developer Bootcamp</p>
-          </div>
-        </div>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={dev} alt="DEV" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">November 2017</p>
-            <p>Started Internship at DEV</p>
-          </div>
-        </div>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={fire} alt="Developer on Fire Podcast Logo" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">December 2017</p>
-            <p>Appeared on the Developer on Fire Podcast</p>
-          </div>
-        </div>
-        <header class="timeline-header">
-          <span class="tag is-primary">2019</span>
-        </header>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={gatsby} alt="Gatsby.js logo" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">January 2019</p>
-            <p>Became a Guest Speaker in a Gatsby.js Webinar</p>
-          </div>
-        </div>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={egghead} alt="Egghead.io logo" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">January 2019</p>
-            <p>Got Offered a Spot to Teach on Egghead.io</p>
-          </div>
-        </div>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={github} alt="GitHub logo" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">March 2019</p>
-            <p>Made GitHub Trending Developers and Repos</p>
-          </div>
-        </div>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={github} alt="GitHub logo" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">May 2019</p>
-            <p>Crossed 3000 stars on The JavaScript Developer's Reading List on GitHub</p>
-          </div>
-        </div>
-        <div class="timeline-item is-danger">
-          <div class="timeline-marker is-warning is-image is-48x48">
-            <img src={codesandbox} alt="Codesandbox logo" />
-          </div>
-          <div class="timeline-content">
-            <p class="heading">May 2019</p>
-            <p>First major PR Merged into Codesandbox</p>
-          </div>
-        </div>
-        <header class="timeline-header">
-          <span class="tag is-medium is-primary">End</span>
-        </header>
-      </div>
-      <Seo facebook={facebook} />
-    </React.Fragment>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2010 - 2011"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            // icon={<WorkIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">Art Director</h3>
+            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+              <a href="https://tiffanywhite.dev">Read Article</a>
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2008 - 2010"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            icon={<FaDev />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2006 - 2008"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            icon={<FaDev />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="April 2013"
+            iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+            icon={<FaDev />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Content Marketing for Web, Mobile and Social Media
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+            <p>Strategy, Social Media</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="November 2012"
+            iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+            icon={<FaDev />}
+          >
+            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+            <p>Creative Direction, User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2002 - 2006"
+            iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+            icon={<FaDev />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Bachelor of Science in Interactive Digital Media Visual Imaging
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+            <p>Creative Direction, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+            icon={<FaDev />}
+          />
+        </VerticalTimeline>
+        <Seo facebook={facebook} />
+      </React.Fragment>
   );
 };
 
