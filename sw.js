ba416ed6e641f8e34a65636c7f2b7b7c9b0bd9ff
Tiffany White <hey@tiffanyrwhite.com>
2019-11-14 21:46:17 -0500

    bump service worker number

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'tiffany-white-blog',
-    suffix: 'v9.6',
+    suffix: 'v9.7',
     precache: 'precache',
     runtime: 'runtime-cache'
   });
