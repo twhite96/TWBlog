7964f210ec02d481b8ade6409b340dbb94575e25
Tiffany White <hey@tiffanyrwhite.com>
2020-04-30 00:55:34 -0400

    Back to Horace theme, fix CSS hydration issues

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'tiffany-white-blog',
-    suffix: 'v10.0',
+    suffix: 'v11.0',
     precache: 'precache',
     runtime: 'runtime-cache'
   });
