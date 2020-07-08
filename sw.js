92d92068e48a711fd66fc8cf7bbe6dd4d8508776
Tiffany White <hey@tiffanyrwhite.com>
2020-07-07 20:06:38 -0400

    Update something

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'tiffany-r-white-blog',
-    suffix: 'v13.0',
+    suffix: 'v13.2',
     precache: 'precache',
     runtime: 'runtime-cache'
   });
